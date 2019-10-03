5686b084e321e95d3ce667ec41a2afb8f37f8f1a
Juan Rosario <juanelias18@hotmail.com>
2019-10-03 19:10:06 -0400

    Avanzo

---
+++ Tarea5-UI/Utilitarios/Utils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tarea5_UI.Utilitarios
+{
+    public static class Utils
+    {
+        public static int ToInt(string valor)
+        {
+            int retorno = 0;
+            int.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+        public static double ToDouble(string valor)
+        {
+            double retorno = 0;
+            double.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+        public static decimal ToDecimal(string valor)
+        {
+            decimal retorno = 0;
+            decimal.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+
+        public static DateTime ToDateTime(string valor)
+        {
+            DateTime retorno = DateTime.Now;
+            DateTime.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+    }
+}
